08528e2d396fec96bd9fb3299cb6d30db4d42b1b
Kirk Tsouros <kirktso@server.fake>
2013-05-17 19:09:56 -0400

    Clean up prompt

--- bash_prompt.sh
+++ bash_prompt.sh
@@ -1,46 +1,78 @@
-# This is stolen from:
-#   https://github.com/gf3/dotfiles
-
-# Sexy Bash Prompt, inspired by "Extravagant Zsh Prompt"
-# Screenshot: http://cloud.gf3.ca/M5rG
-# A big thanks to \amethyst on Freenode
-
-if [[ $COLORTERM = gnome-* && $TERM = xterm ]]  && infocmp gnome-256color >/dev/null 2>&1; then export TERM=gnome-256color
-elif [[ $TERM != dumb ]] && infocmp xterm-256color >/dev/null 2>&1; then export TERM=xterm-256color
+# http://askubuntu.com/questions/233280/gnome-terminal-reports-term-to-be-xterm
+if [ "x$TERM" == "xxterm" ]
+then
+    export TERM=xterm-256color
 fi
 
-if tput setaf 1 &> /dev/null; then
-    tput sgr0
-    if [[ $(tput colors) -ge 256 ]] 2>/dev/null; then
-      MAGENTA=$(tput setaf 9)
-      ORANGE=$(tput setaf 172)
-      GREEN=$(tput setaf 190)
-      PURPLE=$(tput setaf 141)
-      WHITE=$(tput setaf 0)
-    else
-      MAGENTA=$(tput setaf 5)
-      ORANGE=$(tput setaf 4)
-      GREEN=$(tput setaf 2)
-      PURPLE=$(tput setaf 1)
-      WHITE=$(tput setaf 7)
-    fi
-    BOLD=$(tput bold)
-    RESET=$(tput sgr0)
-else
-    MAGENTA="\033[1;31m"
-    ORANGE="\033[1;33m"
-    GREEN="\033[1;32m"
-    PURPLE="\033[1;35m"
-    WHITE="\033[1;37m"
-    BOLD=""
-    RESET="\033[m"
-fi
+RESET="\[\033[0m\]"  # unsets color to term's fg color
 
-parse_git_dirty () {
-  [[ $(git status 2> /dev/null | tail -n1) != "nothing to commit (working directory clean)" ]] && echo "*"
+# regular colors
+BLACK="\[\033[0;30m\]"  # black
+RED="\[\033[0;31m\]"  # red
+GREEN="\[\033[0;32m\]"  # green
+BROWN="\[\033[0;33m\]"  # yellow
+BLUE="\[\033[0;34m\]"  # blue
+PURPLE="\[\033[0;35m\]"  # magenta
+CYAN="\[\033[0;36m\]"  # cyan
+LIGHT_GRAY="\[\033[0;37m\]"  # off white
+
+# emphasized (bolded) colors
+DARK_GRAY="\[\033[1;30m\]" # dark black
+LIGHT_RED="\[\033[1;31m\]" # orange
+LIGHT_GREEN="\[\033[1;32m\]" # light gray
+YELLOW="\[\033[1;33m\]" # light gray
+LIGHT_BLUE="\[\033[1;34m\]" # light gray
+LIGHT_PURPLE="\[\033[1;35m\]" # purple
+LIGHT_CYAN="\[\033[1;36m\]" # light gray
+WHITE="\[\033[1;37m\]" #white
+
+# background colors
+BGK="\[\033[40m\]"
+BGR="\[\033[41m\]"
+BGG="\[\033[42m\]"
+BGY="\[\033[43m\]"
+BGB="\[\033[44m\]"
+BGM="\[\033[45m\]"
+BGC="\[\033[46m\]"
+BGW="\[\033[47m\]"
+
+# https://wiki.archlinux.org/index.php/Color_Bash_Prompt
+#return value visualisation
+#PS1="\$? \$(if [[ \$? == 0 ]]; then echo \"\[\033[0;32m\];)\"; else echo \"\[\033[0;31m\];(\"; fi)\[\033[00m\] : "
+
+case $TERM in
+  xterm*|rxvt*)
+    TITLE_BAR='\[\e]0;\u@\h: \w\a\]'
+    ;;
+  *)
+    TITLE_BAR=""
+    ;;
+esac
+
+user_check() {
+  local UC=$W     # user's color
+  [ $UID -eq "0" ] && UC=$R # root's color
 }
-parse_git_branch () {
+
+# https://gist.github.com/henrik/31631
+function parse_git_dirty () {
+  [[ $(git status 2> /dev/null | tail -n1) != "nothing to commit, working directory clean" ]] && echo "*"
+}
+
+function parse_git_branch () {
   git branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e "s/* \(.*\)/\1$(parse_git_dirty)/"
 }
 
-PS1="\[${BOLD}${MAGENTA}\]\u \[$WHITE\]at \[$ORANGE\]\h \[$WHITE\]in \[$GREEN\]\w\[$WHITE\]\$([[ -n \$(git branch 2> /dev/null) ]] && echo \" on \")\[$PURPLE\]\$(parse_git_branch)\[$WHITE\]\n\$ \[$RESET\]"
+prompt_for_dark() {
+  PS1="${TITLE_BAR}${BROWN}\T ${LIGHT_GREEN}\u ${BLUE}\h ${CYAN}\w ${PURPLE}\$(parse_git_branch)\n${LIGHT_GREEN}\$ ${RESET}"
+}
+
+plain_prompt() {
+  PS1="\u \h \w \$ "
+}
+
+simple_prompt() {
+  PS1="\$ "
+}
+
+prompt_for_dark
