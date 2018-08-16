7716601c7076fd00fa5634122a85e1a28c9c0c70
Kirk Tsouros <kirktso@users.noreply.github.com>
2018-08-16 14:15:44 -0400

    Remove hub alias for git

--- zsh-files/git.zsh
+++ zsh-files/git.zsh
@@ -3,5 +3,3 @@ alias groot='git rev-parse --show-toplevel'
 
 # https://gist.github.com/igrigorik/6666860#gistcomment-1465440
 # alias gh="open `git remote -v | awk '/fetch/{print $2}' | sed -Ee 's#(git@|git://)#http://#' -e 's@com:@com/@'`| head -n1"
-
-eval "$(hub alias -s)"
