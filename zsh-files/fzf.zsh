544b4c0b4b3dd17c732ee90bffb67aad49613c3c
Kirk Tsouros <kirktso@users.noreply.github.com>
2018-08-16 14:13:16 -0400

    Add colors and git functions for fzf

--- zsh-files/fzf.zsh
+++ zsh-files/fzf.zsh
@@ -4,8 +4,12 @@
 # Options to fzf command
 export FZF_COMPLETION_OPTS='+c -x'
 export FZF_DEFAULT_COMMAND='fd --type file --color=always'
-export FZF_DEFAULT_OPTS='--ansi --border'
-
+export FZF_DEFAULT_OPTS='
+    --ansi
+    --border
+    --color fg:188,bg:233,hl:103,fg+:222,bg+:234,hl+:104
+    --color info:183,prompt:110,spinner:107,pointer:167,marker:215
+'
 export FZF_CTRL_T_COMMAND='fd --type file --color=always'
 export FZF_CTRL_T_OPTS="--preview '(highlight -O ansi -l {} 2> /dev/null || cat {} || tree -C {}) 2> /dev/null | head -200'"
 
@@ -22,4 +26,69 @@ _fzf_compgen_path() {
 # Use fd to generate the list for directory completion
 _fzf_compgen_dir() {
   fd --type d --hidden --follow --exclude ".git" . "$1"
+}
+
+# fco() {                                                                                                                                                                                  <<<
+#   local tags branches target
+#   tags=$(git tag | awk '{print "\x1b[31;1mtag\x1b[m\t" $1}') || return
+#   branches=$(
+#     git branch --all | grep -v HEAD             |
+#     sed "s/.* //"    | sed "s#remotes/[^/]*/##" |
+#     sort -u          | awk '{print "\x1b[34;1mbranch\x1b[m\t" $1}') || return
+#   target=$(
+#     (echo "$tags"; echo "$branches") | sed '/^$/d' |
+#     fzf --no-hscroll --reverse --ansi +m -d "\t" -n 2 -q "$*") || return
+#   git checkout $(echo "$target" | awk '{print $2}')
+# }
+
+# GIT heart FZF
+# -------------
+
+is_in_git_repo() {
+  git rev-parse HEAD > /dev/null 2>&1
+}
+
+fzf-down() {
+  fzf --height 50% "$@" --border
+}
+
+gf() {
+  is_in_git_repo || return
+  git -c color.status=always status --short |
+  fzf-down -m --ansi --nth 2..,.. \
+    --preview '(git diff --color=always -- {-1} | sed 1,4d; cat {-1}) | head -500' |
+  cut -c4- | sed 's/.* -> //'
+}
+
+gb() {
+  is_in_git_repo || return
+  git branch -a --color=always | grep -v '/HEAD\s' | sort |
+  fzf-down --ansi --multi --tac --preview-window right:70% \
+    --preview 'git log --oneline --graph --date=short --pretty="format:%C(auto)%cd %h%d %s" $(sed s/^..// <<< {} | cut -d" " -f1) | head -'$LINES |
+  sed 's/^..//' | cut -d' ' -f1 |
+  sed 's#^remotes/##'
+}
+
+gt() {
+  is_in_git_repo || return
+  git tag --sort -version:refname |
+  fzf-down --multi --preview-window right:70% \
+    --preview 'git show --color=always {} | head -'$LINES
+}
+
+gh() {
+  is_in_git_repo || return
+  git log --date=short --format="%C(green)%C(bold)%cd %C(auto)%h%d %s (%an)" --graph --color=always |
+  fzf-down --ansi --no-sort --reverse --multi --bind 'ctrl-s:toggle-sort' \
+    --header 'Press CTRL-S to toggle sort' \
+    --preview 'grep -o "[a-f0-9]\{7,\}" <<< {} | xargs git show --color=always | head -'$LINES |
+  grep -o "[a-f0-9]\{7,\}"
+}
+
+gr() {
+  is_in_git_repo || return
+  git remote -v | awk '{print $1 "\t" $2}' | uniq |
+  fzf-down --tac \
+    --preview 'git log --oneline --graph --date=short --pretty="format:%C(auto)%cd %h%d %s" {1} | head -200' |
+  cut -d$'\t' -f1
 }
