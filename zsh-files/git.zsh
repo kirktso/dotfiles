1111ad03bdcb39f6422dcd6d4e23f99a7f1e0c3a
Kirk Tsouros <kirktso@users.noreply.github.com>
2020-01-28 19:22:39 -0500

    Add environment variable for ghq

--- zsh-files/git.zsh
+++ zsh-files/git.zsh
@@ -1,5 +1,7 @@
 alias gpullall='find . -type d -depth 1 -exec git --git-dir={}/.git --work-tree=$PWD/{} pull origin master \;'
 alias groot='git rev-parse --show-toplevel'
 
+export GHQ_ROOT=/Users/kirk/Developer/remotes
+
 # https://gist.github.com/igrigorik/6666860#gistcomment-1465440
 # alias gh="open `git remote -v | awk '/fetch/{print $2}' | sed -Ee 's#(git@|git://)#http://#' -e 's@com:@com/@'`| head -n1"
